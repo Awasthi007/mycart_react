4c04e2cd8aea090a565df200e8e92570402da704
Shashank Awasthi <awasthi007here@gmail.com>
2022-08-26 17:38:41 +0530

    added the increase quantity functionality

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@
 import './App.css';
 import CartItem from './CartItem';
 
+
+
+
+
 function App() {
   return (
     <div className="App">
+      
       
       <h1>Cart</h1>
       <CartItem/>
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,72 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+    constructor(){
+        super();
+        this.state = {
+            price: 999099,
+            title: 'Mobile Phone',
+            qty: 1,
+            img:''
+        }
+        // this.increaseQty = this.increaseQty.bind(this)
+    }
+    // arrow function binds the object with the function
+    increaseQty = () => {
+        //this.state.qty += 1;
+        console.log(this.state);
+        // set state form 1
+        // this.setState({
+        //     qty: this.state.qty + 1
+        // })
+        // set State form 2
+        this.setState((prevState) => {
+            return{
+                qty: prevState.qty + 1
+            }
+        });
+        // if previous state is requried then use the second form else use the first form
+        
+    }
     render(){
+        const {price, title, qty} = this.state;
         return (
             <div className="cart-item">
                 <div className='left-block'>
-                    <img style={styles.image}/>
+                    <img alt="" style={styles.image}/>
                 </div>
                 <div className='right-block'>
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color: "#777"}}>Rs 999</div>
-                    <div style={{color: "#777"}} >qty: 1</div>
-                    <div className='cart-item-actions'>
 
+                    <div style={{fontSize:25}}>
+                        {title}
+                    </div>
+
+                    <div style={{color: "#777"}}>
+                       Rs {price}
+                    </div>
+
+                    <div style={{color: "#777"}} >
+                        Qty: {qty}
+                    </div>
+
+                    <div className='cart-item-actions'>
+                        {/* Buttons */}
+                        <img 
+                            alt="increase" 
+                            className="action-icons"
+                            src="https://cdn-icons-png.flaticon.com/512/1828/1828926.png"
+                            onClick= {this.increaseQty}
+                        />
+                        <img
+                            alt="decrease" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/992/992683.png"
+                        />
+                        <img 
+                            alt="deleteflat" 
+                            className="action-icons" 
+                            src="https://cdn-icons-png.flaticon.com/512/1214/1214594.png"
+                        />
                     </div>
                 </div>
 
